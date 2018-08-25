767651d8d8c4bfa1cb250c9802c8fe6dd5b98169
scudrt <1208044562@qq.com>
2018-08-25 13:14:18 +0800

    1.1.1 : modify the dytt.js, now the movies' messages are now could be restored

--- dytt/dytt.js
+++ dytt/dytt.js
@@ -1,38 +1,122 @@
+//1.1.1
+var fs = require('fs');
 var http = require('http');
+var async = require('async');
+var events = require('events');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
-var superagent = require('superagent');
 
-var url = 'http://www.dytt8.net/html/gndy/dyzz/index.html'
-var html = '';
+var baseUrl = 'http://www.ygdy8.com';
+var url = 'http://www.ygdy8.com/html/gndy/dyzz/list_23_';
+var path = './movies.json';
+var data = '', pageData;
+var readDone = false;
+var currentPage = 0, currentMovie = 1;
+var loopList = [];
+var timeDelay = 10; //x seconds every page 
+var event = new events.EventEmitter();
 
-function start()
-{
-	http.get(url,function (res)
-	{
-		html = '';
-		res.on('data',function (temp)
-		{
-			html+=iconv.decode(temp,'gb2312');
-		});
-		res.on('end',function ()
-		{
-			solve();
-		})
-	})
-	.on('error',function ()
-	{
-		console.log('请求失败');
-	})
+var Movie = function(title,url,download){
+	return {"title":title,"url":url,"download":download};
 }
 
-function solve()
-{
-	var $ = cheerio.load(html);
-	console.log('部分最新电影名单:(电影天堂)');
-	$('a.ulink').each(function (i,el){
-		console.log(i+'.'+$(this).text());
+function mySleep(tickTime){
+	var t = new Date();
+	while (new Date() - t < tickTime){;}
+}
+
+function preDeal(){
+	var tempUrl = 'http://www.ygdy8.com/html/gndy/dyzz/index.html';
+	var tempHtml = '';
+	http.get(tempUrl,function(res){
+		res.on('data',function(data){
+			tempHtml+=iconv.decode(data,'gb2312');
+		});
+		res.on('end',function(){
+			var $ = cheerio.load(tempHtml);
+			currentPage = Number($('option').last().text());
+		});
 	});
 }
 
-start();
+function save(){ //data saved every page
+	console.log('page '+(currentPage+1)+' saved');
+	data = pageData.concat(data);
+	var temp = JSON.stringify(data).replace(/",/g,'",\n');
+	temp = temp.replace(/},/g,'},\n\n');
+	fs.writeFile(path,'{"page":'+currentPage+',"data":'+temp+'}',function(err){
+		if (err){
+			console.log(err);
+		}
+	});
+}
+
+function movieFetch(){
+	async.forEachSeries(pageData,function(el,fun){
+		mySleep(timeDelay*1000);
+		var currentIndex = pageData.indexOf(el);
+		http.get(el.url,function(res){
+			var html = '';
+			res.on('data',function(data){
+				html+=iconv.decode(data,'gb2312');
+			});
+			res.on('end',function(){
+				console.log((currentIndex+1)+'/'+pageData.length+': '+el.title);
+				var $ = cheerio.load(html);
+				$('td[bgcolor="#fdfddf"]').each(function(){
+					pageData[currentIndex].download.push($(this).text());
+				});
+				if (currentIndex === pageData.length-1){
+					event.emit('moviedone');
+				}
+				fun();
+			});
+		});
+	});
+}
+
+function pageFetch(){ //get the url list from pages
+	console.log('fetching page '+currentPage);
+	var html = '';
+	pageData = [];
+	http.get(url+currentPage+'.html',function(res){
+		res.on('data',function(data){
+			html+=iconv.decode(data,'gb2312');
+		});
+		res.on('end',function(){
+			var $ = cheerio.load(html);
+			$('a.ulink').each(function(){
+				pageData.push(new Movie($(this).text(),baseUrl+$(this).attr('href'),[]));
+			});
+			movieFetch();
+			event.on('moviedone',function(){
+				console.log('page '+currentPage+' done.');
+				--currentPage;
+				save();
+				setTimeout(pageFetch,timeDelay*1000);
+			});
+		});
+	});
+}
+
+//check movies.json
+fs.exists('./movies.json',function(ex){
+	if (ex){
+		console.log('file detected');
+		var temp = JSON.parse(fs.readFileSync(path).toString());
+		currentPage = temp.page;
+		data = temp.data;
+	}
+	else{
+		console.log('no file,initiating');
+		preDeal();
+		fs.writeFile(path,'{"page":['+currentPage+'],"data":[]}',function(err){
+			if (err){
+				console.error(err);
+			}
+		});
+		data=[];
+	}
+});
+
+setTimeout(pageFetch,1500);
--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
 爬虫集合-spider
 
-需要安装node.js+对应模块
+需要安装node.js+额外模块
 
 dytt(电影天堂):
 	爬取电影天堂的最新电影列表并保存对应下载链接。
+	可能会爬到重复的电影，因为电影天堂会重复出电影。
 
-	使用的模块:
-		cheerio + iconv-lite
+	使用的额外模块:
+		cheerio + iconv-lite + async
 
 segmentfault(思否):
 	爬取思否里前端板块的最新新闻。
 
-	使用模块:
+	使用的额外模块:
 		cheerio
